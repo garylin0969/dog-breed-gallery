df0ecb7e94bed9e759ffa2cffe28a565b1e34600
Gary <garylin0969@gmail.com>
2025-03-12 01:23:03 +0800

    feat: add Suspense loading state to BreedList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/organisms/Header';
 import SearchBar from '@/components/molecules/SearchBar';
 import Container from '@/components/atoms/Container';
 import BreedList from '@/components/organisms/BreedList';
+import { Suspense } from 'react';
 
 type SearchParams = Promise<{ [key: string]: string | undefined }>;
 
@@ -15,7 +16,9 @@ export default async function Home(props: { searchParams: SearchParams }) {
                 <SearchBar />
             </Header>
             <Container>
-                <BreedList searchQuery={search} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <BreedList searchQuery={search} />
+                </Suspense>
             </Container>
         </>
     );
