e76a9cc662bdd7150745482b4279f803f070770b
Gary <garylin0969@gmail.com>
2025-03-12 01:06:41 +0800

    feat: add search filtering to BreedList component

--- src/components/organisms/BreedList.tsx
+++ src/components/organisms/BreedList.tsx
@@ -1,13 +1,19 @@
 import { getAllBreeds } from '@/services/dog-apis';
 import BreedItem from '../molecules/BreedItem';
 
-const BreedList = async () => {
+const BreedList = async ({ searchQuery }: { searchQuery?: string }) => {
     const breeds = await getAllBreeds();
+
+    const filteredBreeds = searchQuery
+        ? breeds.filter((breed) => breed?.breedName?.toLowerCase()?.includes(searchQuery?.toLowerCase()))
+        : breeds;
 
     return (
         <ul>
-            {breeds?.map((breed) => (
-                <BreedItem key={breed?.breedName} breed={breed} />
+            {filteredBreeds?.map((breed) => (
+                <li key={breed?.breedName} className="border-b-2 border-gray-200">
+                    <BreedItem breed={breed} />
+                </li>
             ))}
         </ul>
     );
