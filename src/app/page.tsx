1f05898496a5a7161588b8957738cde508641479
Gary <garylin0969@gmail.com>
2025-03-12 01:06:54 +0800

    feat: integrate search functionality into Home page with Header and Container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import Header from '@/components/organisms/Header';
+import SearchBar from '@/components/molecules/SearchBar';
+import Container from '@/components/atoms/Container';
 import BreedList from '@/components/organisms/BreedList';
 
-export default function Home() {
+type SearchParams = Promise<{ [key: string]: string | undefined }>;
+
+export default async function Home(props: { searchParams: SearchParams }) {
+    const searchParams = await props.searchParams;
+    const { search } = searchParams;
+
     return (
         <>
-            <BreedList />
+            <Header>
+                <SearchBar />
+            </Header>
+            <Container>
+                <BreedList searchQuery={search} />
+            </Container>
         </>
     );
 }
