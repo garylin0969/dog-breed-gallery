672a5918ff90365832cbbfe6c001f9bdb5e2f663
Gary <garylin0969@gmail.com>
2025-03-12 01:06:47 +0800

    refactor: make Header component more flexible with children prop

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,18 +1,9 @@
 'use client';
 
-import SearchInput from '../molecules/SearchInput';
+import { ReactNode } from 'react';
 
-const Header = () => {
-    const handleSearch = (value: string) => {
-        // 这里可以处理搜索逻辑，例如触发API调用或更新全局状态
-        console.log('Searching for:', value);
-    };
-
-    return (
-        <header className="h-[50px] w-full bg-[#C4C4C4] px-2.5 py-[9px]">
-            <SearchInput onSearch={handleSearch} />
-        </header>
-    );
+const Header = ({ children }: { children?: ReactNode }) => {
+    return <header className="h-[50px] w-full bg-[#C4C4C4] px-2.5 py-[9px]">{children}</header>;
 };
 
 export default Header;
