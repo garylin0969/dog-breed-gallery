c51965fca9a721ac939ac61ef3fdbd52225b4fd6
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 17:19:58 +0800

    refactor: wrap Header component in container div

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -8,14 +8,16 @@ interface HeaderProps {
 
 const Header = ({ className, children }: HeaderProps) => {
     return (
-        <header
-            className={cn(
-                'sticky top-0 z-10 h-[50px] w-full bg-slate-900/70 font-serif shadow backdrop-blur-md',
-                className
-            )}
-        >
-            <div className="h-full md:mx-auto md:max-w-6xl">{children}</div>
-        </header>
+        <div>
+            <header
+                className={cn(
+                    'sticky top-0 z-10 h-[50px] w-full bg-slate-900/70 font-serif shadow backdrop-blur-md',
+                    className
+                )}
+            >
+                <div className="h-full md:mx-auto md:max-w-6xl">{children}</div>
+            </header>
+        </div>
     );
 };
 
