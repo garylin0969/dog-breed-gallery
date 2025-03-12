53ecfad61d498d5e0504731e5b822d20a4e05948
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 13:43:56 +0800

    refactor: update Modal close icon to use MdClear from react-icons

--- src/components/molecules/Modal.tsx
+++ src/components/molecules/Modal.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from 'react';
-import { IoMdClose } from 'react-icons/io';
-
+import { MdClear } from 'react-icons/md';
 interface ModalProps {
     children?: ReactNode;
     isOpen?: boolean;
@@ -15,7 +14,7 @@ const Modal = ({ children, isOpen = true, onClose }: ModalProps) => {
             <div className="flex flex-col h-full">
                 <div className="flex justify-end p-4">
                     <button className="cursor-pointer" onClick={onClose}>
-                        <IoMdClose className="text-white text-2xl md:text-4xl" />
+                        <MdClear className="text-white text-2xl md:text-4xl" />
                     </button>
                 </div>
                 <div className="flex-1">{children}</div>
