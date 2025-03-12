922f5a914583b9582a11003b53201f3fcf91110f
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 10:48:40 +0800

    refactor: enhance Header component with dynamic className support

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from 'react';
+import cn from '@/lib/cn';
 
-const Header = ({ children }: { children?: ReactNode }) => {
-    return <header className="h-[50px] w-full bg-[#C4C4C4] px-2.5 py-[9px]">{children}</header>;
+interface HeaderProps {
+    className?: string;
+    children?: ReactNode;
+}
+
+const Header = ({ className, children }: HeaderProps) => {
+    return <header className={cn('h-[50px] w-full bg-[#C4C4C4]', className)}>{children}</header>;
 };
 
 export default Header;
