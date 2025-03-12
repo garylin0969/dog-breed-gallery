4ab50bcb4a53668ba8499e7de21c3f1a93290add
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 17:27:47 +0800

    fix: add optional chaining to prevent potential null/undefined errors in BreedList

--- src/components/organisms/BreedList.tsx
+++ src/components/organisms/BreedList.tsx
@@ -9,13 +9,13 @@ const BreedList = async ({ searchQuery }: BreedListProps) => {
     const breeds = await getAllBreeds();
 
     const filteredBreeds = searchQuery
-        ? breeds.filter((breed) => breed.breedName.toLowerCase().includes(searchQuery.toLowerCase()))
+        ? breeds.filter((breed) => breed?.breedName?.toLowerCase()?.includes(searchQuery?.toLowerCase()))
         : breeds;
 
     return (
         <ul>
             {filteredBreeds?.map((breed, index) => (
-                <li key={breed.breedName} className="border-b-2 border-gray-200 hover:bg-gray-200">
+                <li key={breed?.breedName} className="border-b-2 border-gray-200 hover:bg-gray-200">
                     <BreedItem breed={breed} priority={index < 12} />
                 </li>
             ))}
