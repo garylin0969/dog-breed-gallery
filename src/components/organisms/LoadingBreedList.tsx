410d69b6222b2c18967f6482b59d253e7e895dd9
GaryLin <garylin2@anuegroup.com.tw>
2025-03-13 00:39:31 +0800

    refactor: adjust loading placeholder styles in LoadingBreedList component for improved visual consistency

--- src/components/organisms/LoadingBreedList.tsx
+++ src/components/organisms/LoadingBreedList.tsx
@@ -3,8 +3,8 @@ const LoadingBreedList = () => {
         <ul>
             {Array.from({ length: 10 }).map((_, index) => (
                 <li key={index} className="border-b-2 border-gray-200">
-                    <div className="min-h-20 w-full flex items-center gap-5 px-5">
-                        <div className="rounded-full h-[60px] aspect-square bg-gray-200 animate-pulse" />
+                    <div className="h-20 w-full flex items-center gap-5 px-5">
+                        <div className="relative h-[60px] w-[60px] rounded-full overflow-hidden bg-gray-200 animate-pulse" />
                         <div className="h-6 w-40 bg-gray-200 animate-pulse rounded" />
                     </div>
                 </li>
