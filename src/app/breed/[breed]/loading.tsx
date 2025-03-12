ac7b3dd661bd0e121bb1faab596daac80da4647f
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 10:54:09 +0800

    feat: add loading state component for breed detail page

---
+++ src/app/breed/[breed]/loading.tsx
@@ -0,0 +1,28 @@
+import SkeletonGrid from '@/components/molecules/SkeletonGrid';
+import Container from '@/components/atoms/Container';
+import BreedHeader from '@/components/molecules/BreedHeader';
+import Header from '@/components/organisms/Header';
+import { IMAGE_COUNT } from './page';
+
+/**
+ * 品種詳情頁面的加載狀態
+ *
+ * 在頁面內容加載時顯示骨架屏
+ * 使用原子設計架構組織組件
+ */
+const BreedLoading = () => {
+    return (
+        <>
+            <Header className="px-4 flex items-center">
+                <BreedHeader breedName="Loading..." />
+            </Header>
+            <Container>
+                <div className="p-4">
+                    <SkeletonGrid count={IMAGE_COUNT} />
+                </div>
+            </Container>
+        </>
+    );
+};
+
+export default BreedLoading;
