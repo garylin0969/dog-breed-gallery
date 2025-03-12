af64e1fb1c53d7f3ef0af98b7f3cf83ae6c4a2d5
GaryLin <garylin2@anuegroup.com.tw>
2025-03-12 14:10:06 +0800

    refactor: remove BREED_IMAGE_COUNT import and hardcode skeleton grid count

--- src/app/breed/[breed]/loading.tsx
+++ src/app/breed/[breed]/loading.tsx
@@ -2,7 +2,6 @@ import SkeletonGrid from '@/components/molecules/SkeletonGrid';
 import Container from '@/components/atoms/Container';
 import BreedHeader from '@/components/molecules/BreedHeader';
 import Header from '@/components/organisms/Header';
-import { BREED_IMAGE_COUNT } from './page';
 
 const BreedLoading = () => {
     return (
@@ -12,7 +11,7 @@ const BreedLoading = () => {
             </Header>
             <Container>
                 <div className="p-4">
-                    <SkeletonGrid count={BREED_IMAGE_COUNT} />
+                    <SkeletonGrid count={6} />
                 </div>
             </Container>
         </>
